47f0cdc2200a1b8edc24b5894368726bcc7d7a63
ishan058 <ishankhadkaik246@gmail.com>
2023-02-23 10:37:48 +0545

    day 2

--- twodimensional/src/Main.java
+++ twodimensional/src/Main.java
@@ -4,6 +4,21 @@ public class Main {
       int[][] y = {{20,30,50},
                     {30,40,50},
                     {50,60,70}};  //Two dimensional
+        //System.out.println(x[1]);
+
+        //System.out.println(y[0][2]);
+
+        //for(int i = 0; i < 3; i++){
+            //to print one dimensional array
+        //    System.out.println(x[i]);
+        //}
+
+        for(int i = 0; i < x.length; i++){
+            //to print two dimensional array
+            for (int j = 0; j < y[i].length; j++){
+                System.out.println(y[i][j]);
+            }
+        }
 
     }
 }
